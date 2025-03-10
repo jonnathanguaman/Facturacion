35fdd0f10ac0d8c25993911ff0d10152f51e2c7d
jonnathanguaman <jonnathan.guaman.est@tecazuay.edu.ec>
2025-03-10 11:28:05 -0500

    Conexión con eureka

--- spring_cloud_client/src/main/java/com/springcloud/store/spring_cloud_client/SpringCloudClientApplication.java
+++ spring_cloud_client/src/main/java/com/springcloud/store/spring_cloud_client/SpringCloudClientApplication.java
@@ -2,7 +2,9 @@ package com.springcloud.store.spring_cloud_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringCloudClientApplication {
 
