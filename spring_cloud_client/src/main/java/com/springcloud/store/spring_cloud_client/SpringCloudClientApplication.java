472e87b0637c94eff4d58112b3585ff73f289905
jonnathanguaman <jonnathan.guaman.est@tecazuay.edu.ec>
2025-03-12 19:22:26 -0500

    asd

--- spring_cloud_client/src/main/java/com/springcloud/store/spring_cloud_client/SpringCloudClientApplication.java
+++ spring_cloud_client/src/main/java/com/springcloud/store/spring_cloud_client/SpringCloudClientApplication.java
@@ -2,9 +2,9 @@ package com.springcloud.store.spring_cloud_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudClientApplication {
 
--- spring_cloud_product/src/main/java/com/springcloud/store/spring_cloud_product/SpringCloudProductApplication.java
+++ spring_cloud_product/src/main/java/com/springcloud/store/spring_cloud_product/SpringCloudProductApplication.java
@@ -2,7 +2,9 @@ package com.springcloud.store.spring_cloud_product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudProductApplication {
 
